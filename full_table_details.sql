e6ebee4ec5540217e0a68e33c4e255464b62bb68
Alex Shields-Weber <72238329+alliesw@users.noreply.github.com>
2022-03-08 00:54:46 -0500

    Update full_table_details.sql
    -- You can use the sample_table.sql file to test out this procedure. 
    -- Once the table is created, replace 'table_name' with 'test_table' in the code below.

--- full_table_details.sql
+++ full_table_details.sql
@@ -2,7 +2,8 @@ REM Filename      : full_table_details.sql
 REM Description   : Full description of your table - all constraints, indexes, comments & more 
 REM Author        : Alex Shields-Weber      
 
--- You can use the sample_table.sql file to test out this procedure 
+-- You can use the sample_table.sql file to test out this procedure. 
+-- Once the table is created, replace 'table_name' with 'test_table' in the code below. 
 
 SET ECHO OFF 
 SET TERM ON
